ae88ea434c614ba33a0d3fc7703bc895336f27f8
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-03-26 08:37:52 -0300

    Muda pasta clients

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { Student } from '../student';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-clients',
-  templateUrl: './clients.component.html',
-  styleUrl: './clients.component.css'
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrl: './students.component.css'
 })
-export class ClientsComponent {
+export class StudentsComponent {
   students: Student[] = [ ]
 
   formGroupStudent: FormGroup;
