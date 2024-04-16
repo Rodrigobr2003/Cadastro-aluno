fab2b00eb044ddfe3cc3ce381cf913b0ea99d26f
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-04-16 10:52:18 -0300

    :wrench: feat: Add rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StudentsComponent } from './students/students.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'alunos', component: StudentsComponent },
+  { path: 'cursos', component: CursosComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
